c6a1943759e28c64e7d5da9acd2f25a4efd2d73e
bound2 <9380102+bound2@users.noreply.github.com>
2017-05-07 19:04:42 +0300

    basic filteration done

---
+++ economyutils.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def sublist(lst1, lst2):
+    c1 = Counter(lst1)
+    c2 = Counter(lst2)
+    for item, count in c1.items():
+        if count > c2[item]:
+            return False
+    return True
--- redditparser.py
+++ redditparser.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 
 import praw
+import economyutils
+
 
 class RedditParser:
-
     def __init__(self):
         self.reddit = praw.Reddit('bot1')
         self.memeeconomy = self.reddit.subreddit('MemeEconomy')
 
-class Rules:
 
+class Rules:
     KEYWORDS = [
-        ["buy", "buy", "buy"],
-        ["invest", "invest", "invest"],
-        ["high profit"],
-        ["buy quick", "sell quick"],
-        ["great potential", "buy"]
+        list(["buy", "buy", "buy"]),
+        list(["invest", "invest", "invest"]),
+        list(["high profit"]),
+        list(["buy quick", "sell quick"]),
+        list(["great potential", "buy"])
     ]
 
     BAD_KEYWORDS = [
-        ["sell", "sell", "sell"],
-        ["invest", "worthy", "?"],
-        ["should", "i", "invest"],
-        ["could", "i", "get"],
-        ["what", "do", "you"],
-        ["is", "worth", "anything"]
+        list(["sell", "sell", "sell"]),
+        list(["invest", "worthy", "?"]),
+        list(["should", "i", "invest"]),
+        list(["could", "i", "get"]),
+        list(["what", "do", "you"]),
+        list(["is", "worth", "anything"]),
+        list(["safe", "investment", "?"])
     ]
 
     @staticmethod
@@ -32,15 +34,25 @@ class Rules:
         if score >= 500:
             return True
         else:
-            #tite has dank words
-            return False
+            words = list(title.lower().split(' '))
+            good_keywords = Rules.match_to_keywords(words, Rules.KEYWORDS)
+            bad_keywords = Rules.match_to_keywords(words, Rules.BAD_KEYWORDS)
+            return good_keywords and not bad_keywords
+
+    @staticmethod
+    def match_to_keywords(words, validation_keywords):
+        for keywords in validation_keywords:
+            if economyutils.sublist(keywords, words):
+                return True
+
+        return False
+
 
 if __name__ == '__main__':
     reddit_parser = RedditParser()
-    #print reddit_parser.reddit.user.me()
-    for submission in reddit_parser.memeeconomy.hot(limit = 50):
+    for submission in reddit_parser.memeeconomy.hot(limit=50):
+        copy = Rules.should_be_copied(submission.title, submission.score)
         print("Title: ", submission.title)
-        print("Text: ", submission.selftext)
         print("Score: ", submission.score)
+        print("Copy: ", copy)
         print("---------------------------------\n")
-
