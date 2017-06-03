baa18636129924c8ddf0c7c025bb3dc3edbc5734
bound2 <9380102+bound2@users.noreply.github.com>
2017-06-03 19:51:40 +0300

    global processor, instagram configuration in .ini

--- processor/instagramprocessor.py
+++ processor/instagramprocessor.py
@@ -1,34 +1,22 @@
 import atexit
-import os
-from util import fileutils
 from InstagramAPI import InstagramAPI
 
 
 class InstagramProcessor:
-
-    DOWNLOAD_DIR = "..{:s}downloads".format(os.sep)
-
-    def __init__(self):
-        self._instagramApi = InstagramAPI('user', 'password')
+    def __init__(self, username, password):
+        self._instagramApi = InstagramAPI(username, password)
         self._instagramApi.login()
         atexit.register(self.destroy)
 
     def destroy(self):
         self._instagramApi.logout()
 
-    def upload_image(self, image_url, caption='', hashtags=None):
+    def upload_image(self, file_path, caption='', hashtags=None):
         full_caption = self._create_full_caption(caption, hashtags)
-        file_name = fileutils.download_file(url=image_url, destination_folder=InstagramProcessor.DOWNLOAD_DIR)
-        file_path = InstagramProcessor.DOWNLOAD_DIR + os.sep + file_name
-        try:
-            self._instagramApi.uploadPhoto(file_path, caption=full_caption, upload_id=None)
-        finally:
-            fileutils.delete_file(InstagramProcessor.DOWNLOAD_DIR, file_name)
-
+        self._instagramApi.uploadPhoto(file_path, caption=full_caption, upload_id=None)
 
     def _create_full_caption(self, caption, hashtags):
         if hashtags is not None:
             for hashtag in hashtags:
                 caption += ' ' + hashtag
         return caption
-
---
+++ uploader.py
@@ -0,0 +1,63 @@
+import os
+import ConfigParser
+
+from util import fileutils
+from database.memedao import MemeDao
+from database.config import ProductionConfiguration
+from processor.instagramprocessor import InstagramProcessor
+
+
+class BasicUser:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "(username={:s}, password={:s})".format(self.username, self.password)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.username == other.username and self.password == other.password
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(self.username) + hash(self.password)
+
+
+def parse_users(config):
+    users = set()
+    sections = config.sections()
+    for section in sections:
+        user = BasicUser(
+            config.get(section, "username"),
+            config.get(section, "password")
+        )
+        users.add(user)
+    return users
+
+
+DOWNLOAD_DIR = "downloads"
+
+if __name__ == '__main__':
+    meme_dao = MemeDao(ProductionConfiguration())
+    uploadable_memes = meme_dao.find_non_processed()
+
+    config = ConfigParser.ConfigParser()
+    config.read("insta.ini")
+    insta_users = parse_users(config)
+
+    for user in insta_users:
+        insta_processor = InstagramProcessor(user.username, user.password)
+        for meme in uploadable_memes:
+            file_name = fileutils.download_file(url=meme.media_url, destination_folder=DOWNLOAD_DIR)
+            file_path = DOWNLOAD_DIR + os.sep + file_name
+            try:
+                insta_processor.upload_image(file_path)
+                meme_dao.mark_meme_processed(meme.post_id)
+            finally:
+                fileutils.delete_file(DOWNLOAD_DIR, file_name)
